6eba7d7002ce0044544cb1232eb59c40ef50d99e
De-Hype <iamsocialhype@gmail.com>
2023-11-26 14:56:29 -0800

    Adding validation to mongoose schema

--- api/src/models/property.js
+++ api/src/models/property.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const propertySchema = new mongoose.Schema({
     name:{
         type:String,
-        required:true,
+        required:[true,'Name not provided'],
         unique:true
     },
     rooms:{
@@ -18,22 +18,28 @@ const propertySchema = new mongoose.Schema({
 
     address:{
         type:String,
-        required:true,
-        unique:true
+        required:[true,'Address not provided'],
+        unique:  [true,'Address already exist'],
     },
     location:{
         type:String,
         required:true,
     },
     type:{
-        enum:['School', 'House', 'Destination']
+        enum:{values:['School', 'House', 'Destination'],
+        message:`$Does not match either of school, house or destination`
+    }
     },
     price:{
         type:number,
         required:true,
     },
     poster:{
-        required:true,
+        ref:'user',
+        type:mongoose.Types.ObjectId,
+        required:[true, "No product poster provided"]
         //this will make a reference to this poster
     }
 }, {timestamps})
+
+module.exports = mongoose.model('property', propertySchema)
--- api/src/models/user.js
+++ api/src/models/user.js
@@ -22,9 +22,9 @@ const userSchema = new mongoose.Schema({
     },
     property:{
         default:null,
-        ref:'Property',
+        ref:'property',
         type:[mongoose.Types.ObjectId],
         required:true
     }
-})
-module.exports = mongoose.model('User', userSchema)
+}, {timestamps})
+module.exports = mongoose.model('user', userSchema)
