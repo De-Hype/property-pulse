566f33597657223e1f840e21b954f7a34d885014
De-Hype <iamsocialhype@gmail.com>
2023-11-29 21:30:45 -0800

    Simple sign in functionality

--- api/src/controllers/authController.js
+++ api/src/controllers/authController.js
@@ -8,7 +8,7 @@ module.exports.SignUp = catchAsync(async (req, res, next) => {
   let { name, email, username, password } = req.body;
   const findUser = await user.findOne({ email });
   if (findUser) {
-    next(new AppError("User already exist", 402));
+    return next(new AppError("User already exist", 402));
   }
   const salt = await bcrypt.genSalt(8);
   const hashedPassword = await bcrypt.hash(password, salt);
@@ -20,11 +20,27 @@ module.exports.SignUp = catchAsync(async (req, res, next) => {
     password: hashedPassword,
   });
   await createUser.save();
-
-  return res.status(202).json({status:'ok', message:'User account created succesfully', createUser})
+  return res.status(202).json({
+    status: "ok",
+    message: "User account created succesfully",
+    createUser,
+  });
 });
 
-module.exports.SignIn = catchAsync(async (req, res, next) => {});
+module.exports.SignIn = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  const findUser = await user.findOne({ email });
+  if (!findUser) {
+    return next(new AppError("User does not exist", 402));
+  }
+  const user_auth = jwt.sign({ id: user._id }, process.env.Jwt_Secret_Key);
+  res.cookie("user_auth", user_auth, {
+    httpOnly: true,
+  });
+  res
+    .status(202)
+    .json({ status: "ok", message: "User succesfully logged in", findUser });
+});
 
 module.exports.SignOut = catchAsync(async (req, res, next) => {});
 
