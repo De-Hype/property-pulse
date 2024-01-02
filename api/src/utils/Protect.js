c93a18f6b0291b4a273ec8a7b8581a0f1cef411d
De-Hype <iamsocialhype@gmail.com>
2024-01-02 04:40:53 -0800

    created the requireAuth middleware for protection

---
+++ api/src/utils/Protect.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const AppError = require('./AppError');
+
+const requireAuth = (req, res, next)=>{
+    const token = req.cookies.user_auth;
+    if (token){
+        jwt.verify(token, process.env.Jwt_Secret_Key,(err, decodedToken)=>{
+            if (err){
+                return next(new AppError('Invalid token', 401))
+            }
+            console.log(decodedToken)
+            next()
+        })
+    } 
+    return next(new AppError('Unknown user', 401))
+}
+
+module.exports = requireAuth;
