2787bbceabcf91996379f855e86d1233c3bc32ba
De-Hype <iamsocialhype@gmail.com>
2024-01-02 05:19:27 -0800

    Added a nolisting found components

--- vite-project/src/Pages/AboutListing.tsx
+++ vite-project/src/Pages/AboutListing.tsx
@@ -1,9 +1,22 @@
-//Here, we will show the information about a specific listing.
-//All listing details will be shown here
 
+import { AiOutlineHome } from "react-icons/ai"
+import { Link } from "react-router-dom"
+//Here we will have the picture of the listing
+//Here we will have a few text about the listing
 const AboutListing = () => {
   return (
-    <div>AboutListing</div>
+    <div className="flex items-center flex-col gap-6">
+    <Link
+      to="/" 
+      className="flex mt-5 font-bold text-slate-600 items-center gap-1 hover:text-yellow-700 hover:underline"
+    >
+      <AiOutlineHome className="text-2xl" /> <span>Go Home</span>
+    </Link>
+
+    <h3 className="text-center font-bold text-3xl  text-yellow-950">
+      Create A Listing
+    </h3>
+    </div>
   )
 }
 
--- vite-project/src/Pages/MyListing.tsx
+++ vite-project/src/Pages/MyListing.tsx
@@ -1,9 +1,34 @@
 //Here, we will only show the listings of that particular user
 
+import { AiOutlineHome } from "react-icons/ai";
+import { Link } from "react-router-dom";
+
 const MyListing = () => {
   return (
-    <div>MyListing</div>
-  )
-}
+    <>
+      <div className="min-h-screen flex items-center justify-center flex-col">
+        <h3 className="text-center font-bold text-gray-600 text-2xl">No listings found</h3>
+      <Link
+          to="/"
+          className="flex mt-5 font-bold text-slate-600 items-center gap-1 hover:text-yellow-700 hover:underline"
+        >
+          <AiOutlineHome className="text-2xl" /> <span>Go Home</span>
+        </Link>
+      </div>
+      {/* <div className="flex items-center flex-col gap-6">
+        <Link
+          to="/"
+          className="flex mt-5 font-bold text-slate-600 items-center gap-1 hover:text-yellow-700 hover:underline"
+        >
+          <AiOutlineHome className="text-2xl" /> <span>Go Home</span>
+        </Link>
 
-export default MyListing
+        <h3 className="text-center font-bold text-3xl  text-yellow-950">
+          My Listing
+        </h3>
+      </div> */}
+    </>
+  );
+};
+
+export default MyListing;
