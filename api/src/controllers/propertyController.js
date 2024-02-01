d176f609670a6459476f7624f93ffab3244a0dc9
De-Hype <iamsocialhype@gmail.com>
2024-02-01 06:04:50 -0800

    Created UploadImage controller

--- api/src/controllers/propertyController.js
+++ api/src/controllers/propertyController.js
@@ -1,16 +1,42 @@
+const crypto = require("crypto");
+const { initializeApp } = require("firebase/app");
+const {
+  getStorage,
+  ref,
+  getDownloadURL,
+  uploadBytesResumable,
+} = require("firebase/storage");
 const property = require("../models/property");
 const AppError = require("../utils/AppError");
 const catchAsync = require("../utils/catchAsync");
+const firebaseConfig = require("../utils/firebase");
 
 module.exports.CreateListing = catchAsync(async (req, res, next) => {
   const {} = req.body;
+});
+module.exports.UploadImage = catchAsync(async (req, res, next) => {
+  const file = req.file;
+  const filename = crypto.randomBytes(16).toString("hex");
+  initializeApp(firebaseConfig);
+  const storage = getStorage();
+  const metadata = {
+    contentType: req.file.mimetype,
+  };
+  const storageRef = ref(storage, filename);
+  const snapshot = await uploadBytesResumable(storageRef, file, metadata);
+  const downloadURL = await getDownloadURL(snapshot.ref);
+  res.status(200).json({
+    status: "ok",
+    success: true,
+    photo_url: downloadURL,
+    message: "Image stored on firebase successfully",
+  });
 });
 module.exports.UpdateListing = catchAsync(async (req, res, next) => {
   const { id } = req.params;
 });
 module.exports.DeleteListing = catchAsync(async (req, res, next) => {
   const { id } = req.params;
-  
 });
 module.exports.GetListingByUser = catchAsync(async (req, res, next) => {
   const { id } = req.params;
@@ -23,7 +49,6 @@ module.exports.GetListingByUser = catchAsync(async (req, res, next) => {
 
 module.exports.GetHomePageListing = catchAsync(async (req, res, next) => {
   const { id } = req.params;
-
 });
 module.exports.SearchResultListing = catchAsync(async (req, res, next) => {
   const {} = req.body;
