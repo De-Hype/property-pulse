1c2b8dc6f8f97c773e91367d9f442415f9822064
De-Hype <iamsocialhype@gmail.com>
2024-02-01 06:19:44 -0800

    Started work on GetListingByUser

--- api/src/controllers/propertyController.js
+++ api/src/controllers/propertyController.js
@@ -7,12 +7,14 @@ const {
   uploadBytesResumable,
 } = require("firebase/storage");
 const property = require("../models/property");
+const User = require("../models/user");
 const AppError = require("../utils/AppError");
 const catchAsync = require("../utils/catchAsync");
 const firebaseConfig = require("../utils/firebase");
 
 module.exports.CreateListing = catchAsync(async (req, res, next) => {
   const {} = req.body;
+
 });
 module.exports.UploadImage = catchAsync(async (req, res, next) => {
   const file = req.file;
@@ -33,18 +35,24 @@ module.exports.UploadImage = catchAsync(async (req, res, next) => {
   });
 });
 module.exports.UpdateListing = catchAsync(async (req, res, next) => {
-  const { id } = req.params;
+  const userExist = await property.findById({_id: req.params.id}).select("-password");
 });
 module.exports.DeleteListing = catchAsync(async (req, res, next) => {
-  const { id } = req.params;
+  const userExist = await property.findById({_id: req.params.id}).select("-password");
 });
 module.exports.GetListingByUser = catchAsync(async (req, res, next) => {
-  const { id } = req.params;
-  const userExist = await property.findById(id);
+ 
+  const userExist = await User.findById({_id: req.params.id}).select("-password");
   if (userExist) {
     next(new AppError("Invalid ID or user", 401));
   }
-  console.log(userExist);
+  res.status(200).json({
+    status: "ok",
+    success: true,
+    photo_url: downloadURL,
+    message: "Image stored on firebase successfully",
+  });
+  
 });
 
 module.exports.GetHomePageListing = catchAsync(async (req, res, next) => {
