6811ef0554a3320055c628dd33965cc49c40538a
De-Hype <iamsocialhype@gmail.com>
2024-02-01 06:10:05 -0800

    Created routes for upload image

--- api/src/routes/property.routes.js
+++ api/src/routes/property.routes.js
@@ -1,8 +1,10 @@
-const { GetListingByUser, DeleteListing } = require('../controllers/propertyController');
+const { GetListingByUser, DeleteListing, UploadImage } = require('../controllers/propertyController');
+const upload = require('../utils/multer');
 
 const router = require('express').Router();
 
 router.get('/get-user-listing/:id', GetListingByUser);
+router.post('/upload-image', upload.single("image"), UploadImage )
 router.delete('/delete-listing', DeleteListing);
 
 module.exports = router;
