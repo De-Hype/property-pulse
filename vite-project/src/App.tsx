847a6bb5de5cd699eabb1cd5b4b99adf8336163c
David Hype <iamsocialhype@gmail.com>
2024-04-12 04:22:26 -0700

    Adde the private Route in the App.tsx

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Pages/Auth/Login";
 import Register from "./Pages/Auth/Register";
 import Home from "./Pages/Home";
@@ -10,30 +10,48 @@ import AboutListing from "./Pages/AboutListing";
 import UpdateListing from "./Pages/UpdateListing";
 import Contact from "./Pages/Contact";
 import ErrorPage from "./Pages/ErrorPage";
+import { Toaster } from "sonner";
+import ProtectedRoutes from "./utils/ProtectedRoutes";
 
 function App() {
-  
-    return (
-      <div className="App">
-        <BrowserRouter>
-          <Routes>
-            <>
-              <Route  path="/" element={<Home />} />
-              <Route  path="/profile" element={<Profile />} />
-              <Route  path="/create" element={<CreateListing />} />
-              <Route  path="/search" element={<SearchResultListing />} />
-              <Route  path="/contact/:id/:id" element={<Contact />} />
-              <Route  path="/my-listings" element={<MyListing />} />
-              <Route  path="/about/:id" element={<AboutListing />} />
-              <Route  path="/update-listing/:id" element={<UpdateListing />} />
-              <Route path="*" element={<ErrorPage/>} />
-              <Route  path="/sign-in" element={<Login />} />
-              <Route  path="/register" element={<Register />} />
-            </>
-          </Routes>
-        </BrowserRouter>
-      </div>
-    );
-  }
-  export default App;
-  
+  return (
+    <div className="App">
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          unstyled: true,
+          className: {
+            error:
+              "bg-red-400 text-white px-4 py-2 rounded-xl flex items-center gap-2",
+            success:
+              "bg-green-400 text-white px-4 py-2 rounded-xl flex items-center gap-2",
+            info: "bg-blue-400 text-white px-4 py-2 rounded-xl flex items-center gap-2",
+            warning:
+              "bg-blue-400 text-white px-4 py-2 rounded-xl flex items-center gap-2",
+          },
+        }}
+      />
+      <Router>
+        <Routes>
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<ErrorPage />} />
+            <Route path="/sign-in" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/about/:id" element={<AboutListing />} />
+            <Route element={<ProtectedRoutes />}>
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/create" element={<CreateListing />} />
+              <Route path="/search" element={<SearchResultListing />} />
+              <Route path="/contact/:id/:id" element={<Contact />} />
+              <Route path="/my-listings" element={<MyListing />} />
+              <Route path="/update-listing/:id" element={<UpdateListing />} />
+              <Route path="*" element={<ErrorPage />} />
+            </Route>
+          </>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+export default App;
